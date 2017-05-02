ee3bf2e8a4765dc9e4b9c63987d4cd6bc2177fe5
Thiago Bomfim <tbomfim@atlassian.com>
2017-05-02 11:38:15 +1000

    Source code to test the results

---
+++ src/Main.java
@@ -0,0 +1,33 @@
+/**
+ * Created by Thiago Bomfim
+ * on 1/5/17.
+ */
+public class Main {
+    public static void main(String[] args) {
+        byte a;
+        a = 10;
+        System.out.println(a += 3);
+        a = 15;
+        System.out.println(a -= 3);
+        a = 20;
+        System.out.println(a *= 3);
+        a = 25;
+        System.out.println(a /= 3);
+        a = 30;
+        System.out.println(a %= 3);
+        a = 35;
+        // Optional as outside the scope of the exam
+        System.out.println(a &= 3);
+        a = 40;
+        System.out.println(a ^= 3);
+        a = 45;
+        System.out.println(Integer.toBinaryString(a |= 3));
+        a = 50;
+        System.out.println(Integer.toBinaryString(a <<= 3));
+        a = 55;
+        System.out.println((a >>= 3));
+        a = 60;
+        System.out.println((a >>>= 3));
+        // End optional
+    }
+}
